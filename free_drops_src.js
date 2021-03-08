127196099a9cb4397a34f12efa324f535c633e42
Kevin Glowacz <kevin@glowacz.info>
2021-03-08 14:22:17 -0600

    Split into chunks of 8

--- free_drops_src.js
+++ free_drops_src.js
@@ -15,6 +15,17 @@ const table_query = {
   "show_payer": false
 };
 
+const chunk = function(arr, chunk){
+  let chunks = [];
+  for (i=0; i < arr.length; i += chunk) {
+
+    let tempArray;
+    tempArray = arr.slice(i, i + chunk);
+    chunks.push(tempArray);
+  }
+  return chunks;
+};
+
 fetch(get_table_rows_url, {
   method: 'POST',
   body: JSON.stringify(table_query)
@@ -26,10 +37,14 @@ fetch(get_table_rows_url, {
       parseInt(d.current_claimed) < parseInt(d.max_claimable) &&
       (
         d.end_time == 0 || d.end_time > Math.floor((new Date).getTime()/1000)
-      )
+      );
     });
 
     const free_current_ids = free_current.map(d => d.drop_id);
-    const redirect_url = "https://wax.atomichub.io/drops/" + free_current_ids.join('+');
-    window.location = redirect_url;
+    const redirect_url = "https://wax.atomichub.io/drops/";
+    const chunks = chunk(free_current_ids, 8);
+
+    chunks.forEach(chunk => {
+      window.open(redirect_url + chunk.join('+'))
+    });
   });
